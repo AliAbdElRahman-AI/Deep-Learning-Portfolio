b507d00b4fc02a4e54064660720e1651c7e00a85
AliAbdElRahman-AI <aliproa403@gmail.com>
2026-03-27 00:44:03 +0200

    Create 01_Variables.py

---
+++ Python-Mastering/01_Variables.py
@@ -0,0 +1,18 @@
+# ---------------------------------------
+# Learning Python with Elzero Web School
+# Phase 1: Basics (Videos 1 to 7)
+# ---------------------------------------
+
+# 1. Defining Variables
+my_name = "Ali Abd Ul Rahman"
+my_age = 22
+my_field = "AI & Deep Learning"
+
+# 2. Data Types Overview
+print(type(my_name))   # Will show <class 'str'>
+print(type(my_age))    # Will show <class 'int'>
+
+# 3. Concatenation Practice
+# Note: str(my_age) is used because we can't add string to integer directly
+print("Hello, my name is " + my_name + " and I am " + str(my_age) + " years old.")
+print("I'm currently mastering " + my_field + " to build the future!")
